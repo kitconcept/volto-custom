99a448f639232c07fb784c4bfd8309baa012b742
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-06-28 13:04:38 +0200

    Remove customizations, since we will have them in the addon itself

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import { AccordionSchemaEnhancer } from './components/Accordion/schema';
-
 const applyConfig = (config) => {
-  config.blocks.blocksConfig.accordion = {
-    ...config.blocks.blocksConfig.accordion,
-    schemaEnhancer: AccordionSchemaEnhancer,
-  };
-
   return config;
 };
 
